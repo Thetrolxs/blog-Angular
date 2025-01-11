d98e9141a55b261aaa257cfef1dd6b6d9196b007
Thetrolxs <moralesharnisch@gmail.com>
2025-01-10 23:46:14 -0300

    feat: add register and login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './_page/login/login.component';
+import { RegisterComponent } from './_page/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '', pathMatch: 'full'},
+    { path: '', component: LoginComponent},
+    { path: 'register', component: RegisterComponent}
+];
