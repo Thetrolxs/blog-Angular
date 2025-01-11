c04dc7f082a9a583e9074e3c7dbeff1743c249bb
Thetrolxs <moralesharnisch@gmail.com>
2025-01-10 23:59:06 -0300

    feat: add routing posts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './_page/login/login.component';
 import { RegisterComponent } from './_page/register/register.component';
+import { PostsComponent } from './_page/posts/posts.component';
+import { loginGuard } from './_guard/login.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '', pathMatch: 'full'},
     { path: '', component: LoginComponent},
-    { path: 'register', component: RegisterComponent}
+    { path: 'register', component: RegisterComponent},
+    { path: 'posts', component: PostsComponent, canActivate: [loginGuard]}
+
 ];
